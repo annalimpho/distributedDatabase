26f80f8bfbe357e7237019a2e3873a98088cc0ab
annalimpho <118926298+annalimpho@users.noreply.github.com>
2023-04-23 06:48:16 -0700

    Add files via upload

--- scripts(Table Queries).sql
+++ scripts(Table Queries).sql
@@ -80,9 +80,9 @@ VALUES
 
 
 
+/* Tables of paray database */ 
 
-
-CREATE TABLE paray.hospital(
+CREATE TABLE hospital(
     HospitalID INT PRIMARY KEY NOT NULL,
     HospitalName VARCHAR(50),
     HospitalAddress VARCHAR(50),
@@ -91,30 +91,30 @@ CREATE TABLE paray.hospital(
     Location VARCHAR(50)
 );
 
-CREATE TABLE paray.BloodReserve(
+CREATE TABLE BloodReserve(
     BloodID INT PRIMARY KEY NOT NULL,
     GroupQuantity INT,
     ExpiryDate DATE,
     HospitalID INT,
-    FOREIGN KEY (HospitalID) REFERENCES paray.hospital(HospitalID)
+    FOREIGN KEY (HospitalID) REFERENCES hospital(HospitalID)
 );
 
-CREATE TABLE paray.Staff(
+CREATE TABLE Staff(
     EmployeeID INT PRIMARY KEY NOT NULL,
     EmployeeName VARCHAR(50),
     Role VARCHAR(50),
     BloodID INT,
-    FOREIGN KEY (BloodID) REFERENCES paray.BloodReserve(BloodID)
+    FOREIGN KEY (BloodID) REFERENCES BloodReserve(BloodID)
 );
 
-CREATE TABLE paray.BloodType(
+CREATE TABLE BloodType(
     BloodTypeID INT PRIMARY KEY NOT NULL,
     Description VARCHAR(50),
     BloodID INT,
-    FOREIGN KEY (BloodID) REFERENCES paray.BloodReserve(BloodID)
+    FOREIGN KEY (BloodID) REFERENCES BloodReserve(BloodID)
 );
 
-CREATE TABLE paray.Patient(
+CREATE TABLE Patient(
     PatientID INT PRIMARY KEY NOT NULL,
     PatientName VARCHAR(50),
     Gender VARCHAR(50),
@@ -122,45 +122,49 @@ CREATE TABLE paray.Patient(
     DateTransfused DATE,
     QuantityTransfused INT,
     HospitalID INT,
-    FOREIGN KEY (HospitalID) REFERENCES paray.hospital(HospitalID)
+    FOREIGN KEY (HospitalID) REFERENCES hospital(HospitalID)
 );
 
+/* Tables of paray database */ 
 
 
-INSERT INTO paray.hospital (HospitalID, HospitalName, HospitalAddress, HospitalPhone, HospitalEmail, Location)
+/* Populate Tables of paray database */ 
+
+INSERT INTO hospital (HospitalID, HospitalName, HospitalAddress, HospitalPhone, HospitalEmail, Location)
 VALUES
 (1, 'Maluti Hospital', '123 Main St', 55251234, 'info@stmary.com', 'Berea'),
 (2, 'Paray Hospital', '456 Oak Ave', 59355678, 'info@cityhospital.com', 'Thaba-Tseka'),
 (3, 'Mafeteng Hospital', '789 Elm St', 57859012, 'info@universityhospital.com', 'Mafeteng');
 
-INSERT INTO paray.BloodReserve (BloodID, GroupQuantity, ExpiryDate, HospitalID)
+INSERT INTO BloodReserve (BloodID, GroupQuantity, ExpiryDate, HospitalID)
 VALUES
 (1, 10, '2023-06-30', 1),
 (2, 5, '2023-07-31', 2),
 (3, 15, '2023-08-31', 3);
 
-INSERT INTO paray.Staff (EmployeeID, EmployeeName, Role, BloodID)
+INSERT INTO Staff (EmployeeID, EmployeeName, Role, BloodID)
 VALUES
 (1, 'Johanne Lucia', 'Nurse', 1),
 (2, 'Jane Tiko', 'Doctor', 2),
 (3, 'Bobi Mark', 'Lab Technician', 3);
 
-INSERT INTO paray.BloodType (BloodTypeID, Description, BloodID)
+INSERT INTO BloodType (BloodTypeID, Description, BloodID)
 VALUES
 (1, 'A', 1),
 (2, 'B', 2),
 (3, 'O', 3);
 
-INSERT INTO paray.Patient (PatientID, PatientName, Gender, ContactInfo, DateTransfused, QuantityTransfused, HospitalID)
+INSERT INTO Patient (PatientID, PatientName, Gender, ContactInfo, DateTransfused, QuantityTransfused, HospitalID)
 VALUES
 (1, 'Sarah Leeto', 'Female', 55501111, '2023-04-01', 2, 1),
 (2, 'Mike Johnson', 'Male', 55562222, '2023-04-15', 1, 2),
-(3, 'Emily Tebisa', 'Female', 55513333, '2023-04-20', 3, 3)
+(3, 'Emily Tebisa', 'Female', 55513333, '2023-04-20', 3, 3);
 
+/* Populate Tables of paray database */ 
 
+/* Tables of maluti database */ 
 
-
-CREATE TABLE maluti.hospital(
+CREATE TABLE hospital(
 HospitalID INT PRIMARY KEY NOT NULL,
 HospitalName VARCHAR(50),
 HospitalAddress VARCHAR(50),
@@ -169,76 +173,79 @@ HospitalEmail VARCHAR(50),
 Location VARCHAR(50)
 );
 
-CREATE TABLE maluti.BloodReserve(
+CREATE TABLE BloodReserve(
 BloodID INT PRIMARY KEY NOT NULL,
 GroupQuantity INT,
-ExpiryDate INT,
+ExpiryDate DATE,
 HospitalID INT,
-FOREIGN KEY (HospitalID) REFERENCES maluti.hospital(HospitalID)
+FOREIGN KEY (HospitalID) REFERENCES hospital(HospitalID)
 );
 
-CREATE TABLE maluti.Staff(
+CREATE TABLE Staff(
 EmployeeID INT PRIMARY KEY NOT NULL,
 EmployeeName VARCHAR(50),
 Role VARCHAR(50),
 BloodID INT,
-FOREIGN KEY (BloodID) REFERENCES maluti.BloodReserve(BloodID)
+FOREIGN KEY (BloodID) REFERENCES BloodReserve(BloodID)
 );
 
-CREATE TABLE maluti.BloodType(
+CREATE TABLE BloodType(
 BloodTypeID INT PRIMARY KEY NOT NULL,
 Description VARCHAR(50),
 BloodID INT,
-FOREIGN KEY (BloodID) REFERENCES maluti.BloodReserve(BloodID)
+FOREIGN KEY (BloodID) REFERENCES BloodReserve(BloodID)
 );
 
-CREATE TABLE maluti.Patient(
+CREATE TABLE Patient(
 PatientID INT PRIMARY KEY NOT NULL,
 PatientName VARCHAR(50),
 Gender VARCHAR(50),
 ContactInfo INT,
-DateTransfused INT,
+DateTransfused DATE,
 QuantityTransfused INT,
 HospitalID INT,
-FOREIGN KEY (HospitalID) REFERENCES maluti.hospital(HospitalID)
+FOREIGN KEY (HospitalID) REFERENCES hospital(HospitalID)
 );
 
+/* Tables of maluti database */
 
+/* Populate Tables of maluti database */ 
 
-INSERT INTO maluti.hospital (HospitalID, HospitalName, HospitalAddress, HospitalPhone, HospitalEmail, Location)
-VALUES (1, 'Mafeteng Hospital', '123 Main Street', 555-1234, 'maluti@hospital.com', 'Mafeteng'),
-       (2, 'Maluti Hospital', '456 Oak Avenue', 555-5678, 'mmc@hospital.com', 'Maluti'),
-       (3, 'Paray Hospital', '789 Maple Drive', 555-9012, 'mch@hospital.com', 'Paray'),
-       (4, 'Mafeteng Hospital', '321 Elm Street', 555-3456, 'mrh@hospital.com', 'Mafeteng');
+INSERT INTO hospital (HospitalID, HospitalName, HospitalAddress, HospitalPhone, HospitalEmail, Location)
+VALUES (1, 'Mafeteng Hospital', '123 Main Street', 55541234, 'maluti@hospital.com', 'Mafeteng'),
+       (2, 'Maluti Hospital', '456 Oak Avenue', 55565678, 'mmc@hospital.com', 'Maluti'),
+       (3, 'Paray Hospital', '789 Maple Drive', 55579012, 'mch@hospital.com', 'Paray'),
+       (4, 'Mafeteng Hospital', '321 Elm Street', 55583456, 'mrh@hospital.com', 'Mafeteng');
        
-INSERT INTO maluti.BloodReserve (BloodID, GroupQuantity, ExpiryDate, HospitalID)
-VALUES (1, 50, 2024-05-31, 1),
-       (2, 75, 2024-06-30, 2),
-       (3, 100, 2024-07-31, 3),
-       (4, 125, 2024-08-31, 4);
+INSERT INTO BloodReserve (BloodID, GroupQuantity, ExpiryDate, HospitalID)
+VALUES (1, 50, '2022-05-31', 1),
+       (2, 75, '2022-06-30', 2),
+       (3, 100, '2022-07-31', 3),
+       (4, 125, '2022-08-31', 4);
 
-INSERT INTO maluti.Staff (EmployeeID, EmployeeName, Role, BloodID)
+INSERT INTO Staff (EmployeeID, EmployeeName, Role, BloodID)
 VALUES (1, 'Thabo Smith', 'Nurse', 1),
        (2, 'Jane David', 'Doctor', 2),
        (3, 'Bokang Lieta', 'Lab Technician', 3),
        (4, 'Sara Williams', 'Admin Assistant', 4);
 
-INSERT INTO maluti.BloodType (BloodTypeID, Description, BloodID)
+INSERT INTO BloodType (BloodTypeID, Description, BloodID)
 VALUES (1, 'A', 1),
        (2, 'B', 2),
        (3, 'AB', 3),
        (4, 'O', 4);
 
-INSERT INTO maluti.Patient (PatientID, PatientName, Gender, ContactInfo, DateTransfused, QuantityTransfused, HospitalID)
-VALUES (1, 'Tom Jones', 'Male', 55341212, 2022-04-10, 2, 1),
-       (2, 'Lineo Smith', 'Female', 59153434, 2022-05-15, 3, 2),
-       (3, 'Joseph Borikhoe', 'Male', 58755656, 2022-06-20, 1, 3),
-       (4, 'Susan Lekau', 'Female', 53557878, 2022-07-25, 4, 4);
+INSERT INTO Patient (PatientID, PatientName, Gender, ContactInfo, DateTransfused, QuantityTransfused, HospitalID)
+VALUES (1, 'Tom Jones', 'Male', 55341212, '2022-04-10', 2, 1),
+       (2, 'Lineo Smith', 'Female', 59153434, '2022-05-15', 3, 2),
+       (3, 'Joseph Borikhoe', 'Male', 58755656,'2022-06-20', 1, 3),
+       (4, 'Susan Lekau', 'Female', 53557878, '2022-07-25', 4, 4);
 
+/* Populate Tables of maluti database */ 
 
+/* Tables of mafeteng database */
 
-
-CREATE TABLE mafeteng.hospital(
+CREATE TABLE hospital(
 HospitalID INT PRIMARY KEY NOT NULL,
 HospitalName VARCHAR(50),
 HospitalAddress VARCHAR(50),
@@ -247,67 +254,69 @@ HospitalEmail VARCHAR(50),
 Location VARCHAR(50)
 );
 
-CREATE TABLE mafeteng.BloodReserve(
+CREATE TABLE BloodReserve(
 BloodID INT PRIMARY KEY NOT NULL,
 GroupQuantity VARCHAR(50),
-ExpiryDate INT,
+ExpiryDate DATE,
 HospitalID INT,
-FOREIGN KEY (HospitalID) REFERENCES mafeteng.hospital(HospitalID)
+FOREIGN KEY (HospitalID) REFERENCES hospital(HospitalID)
 );
 
-CREATE TABLE mafeteng.Staff(
+CREATE TABLE Staff(
 EmployeeID INT PRIMARY KEY NOT NULL,
 EmployeeName VARCHAR(50),
 Role VARCHAR(50),
 BloodID INT,
-FOREIGN KEY (BloodID) REFERENCES mafeteng.BloodReserve(BloodID)
+FOREIGN KEY (BloodID) REFERENCES BloodReserve(BloodID)
 );
 
-CREATE TABLE mafeteng.BloodType(
+CREATE TABLE BloodType(
 BloodTypeID INT PRIMARY KEY NOT NULL,
 Description VARCHAR(50),
 BloodID INT,
-FOREIGN KEY (BloodID) REFERENCES mafeteng.BloodReserve(BloodID)
+FOREIGN KEY (BloodID) REFERENCES BloodReserve(BloodID)
 );
 
-CREATE TABLE mafeteng.Patient(
+CREATE TABLE Patient(
 PatientID INT PRIMARY KEY NOT NULL,
 PatientName VARCHAR(50),
 Gender VARCHAR(50),
 ContactInfo INT,
-DateTransfused INT,
+DateTransfused DATE,
 QuantityTransfused INT,
 HospitalID INT,
-FOREIGN KEY (HospitalID) REFERENCES mafeteng.hospital(HospitalID)
-);
+FOREIGN KEY (HospitalID) REFERENCES hospital(HospitalID)
+); 
 
+/* Tables of mafeteng database */
 
+/* Populate Tables of mafeteng database */
 
-INSERT INTO mafeteng.hospital (HospitalID, HospitalName, HospitalAddress, HospitalPhone, HospitalEmail, Location) VALUES
-(1, 'Mafeteng Hospital', '123 Main Street', 555-1234, 'info@mafehospital.com', 'Mafeteng'),
-(2, 'Maluti Hospital', '456 Oak Ave', 555-5678, 'info@bereahospital.com', 'Berea'),
-(3, 'Paray Hospital', '789 Maple Blvd', 555-9012, 'info@teyatehospital.com', 'Thaba-Tseka');
+INSERT INTO hospital (HospitalID, HospitalName, HospitalAddress, HospitalPhone, HospitalEmail, Location) VALUES
+(1, 'Mafeteng Hospital', '123 Main Street', 55451234, 'info@mafehospital.com', 'Mafeteng'),
+(2, 'Maluti Hospital', '456 Oak Ave', 59555678, 'info@bereahospital.com', 'Berea'),
+(3, 'Paray Hospital', '789 Maple Blvd', 58559012, 'info@teyatehospital.com', 'Thaba-Tseka');
 
-INSERT INTO mafeteng.BloodReserve (BloodID, GroupQuantity, ExpiryDate, HospitalID) VALUES
-(1, 'O', 2024-05-01, 1),
-(2, 'O', 2024-06-01, 2),
-(3, 'A', 2024-07-01, 3);
+INSERT INTO BloodReserve (BloodID, GroupQuantity, ExpiryDate, HospitalID) VALUES
+(1, 'O', '2024-05-01', 1),
+(2, 'O', '2024-06-01', 2),
+(3, 'A', '2024-07-01', 3);
 
-INSERT INTO mafeteng.Staff (EmployeeID, EmployeeName, Role, BloodID) VALUES
+INSERT INTO Staff (EmployeeID, EmployeeName, Role, BloodID) VALUES
 (1, 'Tumelo Seeta', 'Nurse', 1),
 (2, 'Katleho Setulo', 'Doctor', 2),
 (3, 'Thabo Kausi', 'Technician', 3);
 
-INSERT INTO mafeteng.BloodType (BloodTypeID, Description, BloodID) VALUES
+INSERT INTO BloodType (BloodTypeID, Description, BloodID) VALUES
 (1, 'Whole Blood', 1),
 (2, 'Platelets', 2),
 (3, 'Plasma', 3);
 
-INSERT INTO mafeteng.Patient (PatientID, PatientName, Gender, ContactInfo, DateTransfused, QuantityTransfused, HospitalID) VALUES
-(1, 'Sello Mali', 'Female', 55524321, 2022-01-01, 2, 1),
-(2, 'Tolosi Maili', 'Male', 55528765, 2022-02-01, 1, 2),
-(3, 'Jumama Leeli', 'Female', 55522468, 2022-03-01, 3, 3);
+INSERT INTO Patient (PatientID, PatientName, Gender, ContactInfo, DateTransfused, QuantityTransfused, HospitalID) VALUES
+(1, 'Sello Mali', 'Female', 55524321, '2022-01-01', 2, 1),
+(2, 'Tolosi Maili', 'Male', 55528765, '2022-02-01', 1, 2),
+(3, 'Jumama Leeli', 'Female', 55522468, '2022-03-01', 3, 3);
 
-
+/* Populate Tables of mafeteng database */
 
 
